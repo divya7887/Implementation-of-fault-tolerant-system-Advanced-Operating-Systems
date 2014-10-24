b26807306a00c1d19e65db645d245e5bc8f1a6d1
divya7887 <divya7887@users.noreply.github.com>
2014-10-23 22:14:57 -0500

    Create Votes.java

---
+++ Votes.java
@@ -0,0 +1,29 @@
+//package replicamain;
+public class Votes {
+
+	int nodeId;
+	int votes;
+	
+	
+	public Votes(int nodeId, int votes) {
+		
+		this.nodeId = nodeId;
+		this.votes = votes;
+	}
+	public int getNodeId() {
+		return nodeId;
+	}
+	public void setNodeId(int nodeId) {
+		this.nodeId = nodeId;
+	}
+	public int getVotes() {
+		return votes;
+	}
+	public void setVotes(int votes) {
+		this.votes = votes;
+	}
+	
+	
+	
+	
+}
